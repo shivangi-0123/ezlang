20682eaa982a0f2a3b963fcf75c34bd21fb19a2c
shivangi-0123 <shivangitripathi1011@gmail.com>
2025-04-24 23:06:30 +0530

    Update LandingPage

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -47,7 +47,7 @@ likho("🎉 Series complete!")`;
                             <button className="primary-button" onClick={() => navigate("/editor")}>
                                 Explore Playground
                             </button>
-                            <a href="https://github.com/amanverma-765/Ez-Lang" className="secondary-button">
+                            <a href="https://github.com/shivangi-0123/ezlang" className="secondary-button">
                                 View on GitHub
                             </a>
                         </div>
